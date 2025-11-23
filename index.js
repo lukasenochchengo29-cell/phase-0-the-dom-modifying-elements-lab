31ea71f14ae8341c1e89a634182aaea38427c1e1
lukas enoch chengo <lukas.enoch.chengo29@gmail.com>
2025-11-24 01:12:30 +0300

    thats me

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Write your code here!
+// Write your code here
+// Remove the <main> element with id="main"
+const main = document.getElementById('main');
+main.remove();
+
+// Create the new <h1> element
+const newHeader = document.createElement('h1');
+
+// Give it the required id
+newHeader.id = 'victory';
+
+// Add the required text (replace YOUR-NAME with your actual name)
+newHeader.textContent = "Lukas is the champion";
+
+// Append it to the document
+document.body.append(newHeader);
+
+
